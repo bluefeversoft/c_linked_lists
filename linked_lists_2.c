9e76f205bb7e02275e83ca3d56620a053b9b71df
Bluefever <bluefeversoft@gmail.com>
2019-03-14 17:43:59 +0100

    ch2

---
+++ linked_lists_2.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct sPerson {
+	int age;
+	struct sPerson *nextInLine;
+};
+
+void printPerson(struct sPerson *person, const char *comment)
+{
+    printf("%s age:%d address:%X nextInLine:%X\n", comment,
+    person->age, person, person->nextInLine);
+}
+
+struct sPerson *getNewPerson(const int age) 
+{
+    struct sPerson *newPerson = NULL;
+    newPerson = (struct sPerson*)malloc(sizeof(struct sPerson));
+	newPerson->nextInLine = NULL;
+	newPerson->age = age;
+    printf("new person at %X\n", newPerson);
+    return newPerson;
+}
+
+int main() 
+{
+	printf("** START **\n");
+
+    struct sPerson *first = NULL;
+    struct sPerson *second = NULL;
+
+    printf("first %X\n", first);
+    printf("second %X\n", first);
+
+    first = getNewPerson(125);
+    second = getNewPerson(50);
+
+    printPerson(first, "First");
+    printPerson(second, "Second");
+
+    first->nextInLine = second;
+    printPerson(first, "First After Assign");
+    printPerson(first->nextInLine, "first->nextInLine");
+    printPerson(second, "Second");
+
+    free(second);
+    second = NULL;
+    free(first);
+    first = NULL;
+
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
