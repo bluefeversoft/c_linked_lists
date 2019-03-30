210aa59a5ece27061444d9701694222a19bebe97
Bluefever <bluefeversoft@gmail.com>
2019-03-30 08:41:29 +0100

    lesson 1 adjustments

--- linked_lists_1.c
+++ linked_lists_1.c
@@ -3,17 +3,29 @@
 
 struct sPerson {
 	int age;
-	struct sPerson *nextInLine;
 };
 
 struct sPerson *getNewPerson(const int age) 
 {
     struct sPerson *newPerson = NULL;
     newPerson = malloc(sizeof(struct sPerson));
-	newPerson->nextInLine = NULL;
 	newPerson->age = age;
-    printf("new person at %p\n", newPerson);
+    printf("created new person at %p\n", newPerson);
     return newPerson;
+}
+
+void printPerson(const struct sPerson *person)
+{
+    if (person == NULL)
+    {
+        printf("Printing NULL sPerson\n");
+    }
+    else
+    {
+        printf("age:%d address:%p\n", 
+            person->age, 
+            person);
+    }
 }
 
 int main() 
@@ -21,14 +33,16 @@ int main()
 	printf("\n\n** START **\n\n");
 
     struct sPerson *first = NULL;
-    printf("first %p\n", first);
+    struct sPerson *second = NULL;
+
+    printPerson(first);
+    printPerson(second);
 
     first = getNewPerson(125);
+    second = getNewPerson(100);
 
-    printf("age:%d address:%p nextInLine:%p\n", 
-        first->age, 
-        first, 
-        first->nextInLine);
+    printPerson(first);
+    printPerson(second);
 
 	return 0;
 }
